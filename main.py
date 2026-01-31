7a51653725b4d5b868fe16e01258794c4dff19a1
MED-System <mohamed.edris1247@gmail.com>
2026-01-31 22:13:54 +0000

    First Day Code

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-name = "George"
-age = "70"
-print(" there was once old man called " + name)
-print(" he was " + age +" years old")
-print(" he really liked his "+ name)
-print(" but he didnt like being " + age)
+phrase =  "girrafe academy"
+print(phrase.replace("girrafe", "Elephant"))
